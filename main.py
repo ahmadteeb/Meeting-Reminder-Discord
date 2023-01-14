ecfe93b7db7e6488b893db298017656683b2ce39
Ahmad El-khatib <ahmadelkhatib1998@gmail.com>
2023-01-14 18:16:32 +0300

    Add Connected Message

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import discord
 import os
+from dotenv import load_dotenv
 from discord import client
 from discord.ext import tasks
 from aioschedule import every, run_pending
 from discord.ext import commands
 
+load_dotenv()
+
 intents = discord.Intents.default()
 intents.members = True
-Client = commands.Bot(command_prefix = '#', intents=intents)
+client = commands.Bot(command_prefix = '#', intents=intents)
 
 
-@Client.event
+@client.event
 async def on_ready():
     day = os.environ['day']
     if(day == "saturday"):
@@ -32,15 +35,28 @@ async def on_ready():
 
 @tasks.loop(seconds=0)
 async def loop():
-    await Client.wait_until_ready()
+    await client.wait_until_ready()
     await run_pending()
 
 async def Remind_Members():
-    Members = Client.get_guild(int(os.environ['server_id'])).members
+    Members = client.get_guild(int(os.environ['server_id'])).members
     for member in Members:
         if(not member.bot):
             embed=discord.Embed(title=os.environ['title'], description=os.environ['desc'])
             await member.send(embed=embed)
 
+@client.event
+async def on_voice_state_update(member, before, after):
+    if(before.channel == None):
+        Members = client.get_guild(int(os.environ['server_id'])).members
+
+        title = f"{ member.name } Connected"
+        desc = f"Channel: { after.channel }"
+        embed=discord.Embed(title= title, description=desc)
+
+        for mem in Members:
+            if(not mem.bot and mem.id != member.id):
+                await mem.send(embed=embed)
+
 if __name__ == "__main__":
-    Client.run(os.environ['token'])
+    client.run(os.environ['token'])
